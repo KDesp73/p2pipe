baf3ab7e25b9b5f0c643af521a5421ce401cedd4
Konstantinos Despoinidis <kdesp2003@gmail.com>
2025-11-02 21:53:57 +0200

    docs: added CHANGELOG.md

--- src/p2pipe/handshake.c
+++ src/p2pipe/handshake.c
@@ -11,14 +11,14 @@
 #include <netdb.h>
 
 ssize_t udp_handshake_blocking(const char *server_ip,
-                               unsigned short server_port,
-                               const char *payload,
-                               char *out_buf,
-                               size_t out_buf_size,
-                               unsigned timeout_sec,
-                               unsigned max_retries,
-                               unsigned short client_port,
-                               int *out_sock)
+        unsigned short server_port,
+        const char *payload,
+        char *out_buf,
+        size_t out_buf_size,
+        unsigned timeout_sec,
+        unsigned max_retries,
+        unsigned short client_port,
+        int *out_sock)
 {
     if (!server_ip || !payload || !out_buf || out_buf_size == 0) {
         errno = EINVAL;
